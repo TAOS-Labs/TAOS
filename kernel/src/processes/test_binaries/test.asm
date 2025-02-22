e81a6824f0e0aaebb917aa6948cf57a5d651af9f
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2025-02-22 16:44:19 -0600

    Added arguments, untested

--- kernel/src/processes/test_binaries/test.asm
+++ kernel/src/processes/test_binaries/test.asm
@@ -2,14 +2,8 @@ section .text
     global _start
 
 _start:
-  mov rdi, 1
-  mov rsi, 2
-  mov rdx, 3
-  mov r10, 4
-  mov r9, 5
-  mov r8, 6
-  mov rax, 1 ; EXIT
-  syscall
+    mov rax, 1 ; EXIT
+    syscall
 
 _loop:
   jmp _loop
