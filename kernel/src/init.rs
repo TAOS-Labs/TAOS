b96cfc53ee5b021203660d84b8ee8b74b720b74b
Pranay Gosar <pgosarsteam@gmail.com>
2025-02-27 18:17:01 -0600

    Reenable explicit CPU event scheduling onto other cores (#57)
    * fix cpu event registration
    
    * move to wake cores

--- kernel/src/init.rs
+++ kernel/src/init.rs
@@ -54,7 +54,6 @@ pub fn init() -> u32 {
     debug!("Waking cores");
     let bsp_id = wake_cores();
 
-    register_event_runner();
     idt::enable();
 
     bsp_id
@@ -84,7 +83,6 @@ unsafe extern "C" fn secondary_cpu_main(cpu: &Cpu) -> ! {
         core::hint::spin_loop();
     }
 
-    register_event_runner();
     idt::enable();
 
     debug!("AP {} entering event loop", cpu.id);
@@ -114,6 +112,7 @@ fn wake_cores() -> u32 {
     while CPU_COUNT.load(Ordering::SeqCst) < cpu_count - 1 {
         core::hint::spin_loop();
     }
+    register_event_runner();
 
     BOOT_COMPLETE.store(true, Ordering::SeqCst);
 
