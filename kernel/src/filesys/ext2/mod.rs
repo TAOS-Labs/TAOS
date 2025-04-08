0b2726f6df576a47460b9387f561c88e203c39b8
Joseph Wilson <josephcwilson47@gmail.com>
2025-04-08 13:08:57 -0500

    Add tests for real now

--- kernel/src/filesys/ext2/mod.rs
+++ kernel/src/filesys/ext2/mod.rs
@@ -11,3 +11,26 @@ pub fn get_current_time() -> u32 {
     // For now, return a dummy
     1234567890
 }
+
+#[cfg(test)]
+mod tests {
+    use alloc::sync::Arc;
+
+    use crate::devices::sd_card::SD_CARD;
+
+    use super::filesystem::Ext2;
+
+    const MEDIUM_FILE: &[u8] = include_bytes!("../../../../resources/fonts/Comfortaa-Regular.ttf");
+
+    // Size and layout tests
+    #[test_case]
+    async fn fat_test_all() {
+        let sd_card_lock = SD_CARD.lock();
+        let sd_card = sd_card_lock.clone().unwrap();
+        let sd_arc = Arc::new(sd_card);
+        let fs = Ext2::new(sd_arc).await.unwrap();
+        fs.mount().await.unwrap();
+        let file = fs.read_file("/fonts/Comfortaa-Regular.ttf").await.unwrap();
+        assert!(file == MEDIUM_FILE);
+    }
+}
