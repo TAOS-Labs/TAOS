62cb847e545ab28d51f7490453cd56ff39f653af
pgosar <pgosarsteam@gmail.com>
2025-04-17 15:01:23 -0500

    add test case to open c executable from file and run it

--- kernel/src/processes/mod.rs
+++ kernel/src/processes/mod.rs
@@ -12,12 +12,15 @@ pub fn init(cpu_id: u32) {
 
 #[cfg(test)]
 mod tests {
+    use alloc::vec;
+
     use crate::{
         constants::processes::TEST_SIMPLE_PROCESS,
         events::{
             current_running_event, futures::await_on::AwaitProcess, get_runner_time,
             schedule_process,
         },
+        filesys::{FileSystem, OpenFlags, FILESYSTEM},
         processes::process::create_process,
     };
 
@@ -30,6 +33,40 @@ mod tests {
             get_runner_time(3_000_000_000),
             current_running_event().unwrap(),
         ) // TODO how to get event corresponding to testcase?
+        .await;
+        assert!(waiter.is_ok());
+    }
+
+    #[test_case]
+    async fn test_simple_c_ret() {
+        let fs = FILESYSTEM.get().unwrap();
+        let fd = {
+            fs.lock()
+                .open_file(
+                    "/executables/ret",
+                    OpenFlags::O_RDONLY | OpenFlags::O_WRONLY,
+                )
+                .await
+                .expect("Could not open file")
+        };
+
+        let mut buffer = vec![0u8; 4096];
+        let bytes_read = {
+            fs.lock()
+                .read_file(fd, &mut buffer)
+                .await
+                .expect("Failed to read file")
+        };
+
+        let buf = &buffer[..bytes_read];
+
+        let pid = create_process(buf);
+        schedule_process(pid);
+        let waiter = AwaitProcess::new(
+            pid,
+            get_runner_time(3_000_000_000),
+            current_running_event().unwrap(),
+        )
         .await;
         assert!(waiter.is_ok());
     }
