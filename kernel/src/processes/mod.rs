24dd31beecadce65105332da5e19c04c6b32ee23
Joseph Wilson <josephcwilson47@gmail.com>
2025-04-25 16:25:59 -0500

    Removed test_simple_c_ret

--- kernel/src/processes/mod.rs
+++ kernel/src/processes/mod.rs
@@ -12,18 +12,14 @@ pub fn init(cpu_id: u32) {
 
 #[cfg(test)]
 mod tests {
-    use alloc::vec;
 
     use crate::{
         constants::processes::TEST_SIMPLE_PROCESS,
-        debug,
         events::{
             current_running_event, futures::await_on::AwaitProcess, get_runner_time,
             schedule_process,
         },
-        filesys::{FileSystem, OpenFlags, FILESYSTEM},
         processes::process::create_process,
-        syscalls::memorymap::{sys_mmap, MmapFlags, ProtFlags},
     };
 
     #[test_case]
@@ -39,46 +35,46 @@ mod tests {
         assert!(waiter.is_ok());
     }
 
-    #[test_case]
-    async fn test_simple_c_ret() {
-        let fs = FILESYSTEM.get().unwrap();
-        let fd = {
-            fs.lock()
-                .open_file(
-                    "/executables/ret",
-                    OpenFlags::O_RDONLY | OpenFlags::O_WRONLY,
-                )
-                .await
-                .expect("Could not open file")
-        };
-        sys_mmap(
-            0x9000,
-            0x1000,
-            ProtFlags::PROT_EXEC.bits(),
-            MmapFlags::MAP_FILE.bits(),
-            fd as i64,
-            0,
-        );
+    // #[test_case]
+    // async fn test_simple_c_ret() {
+    //     let fs = FILESYSTEM.get().unwrap();
+    //     let fd = {
+    //         fs.lock()
+    //             .open_file(
+    //                 "/executables/ret",
+    //                 OpenFlags::O_RDONLY | OpenFlags::O_WRONLY,
+    //             )
+    //             .await
+    //             .expect("Could not open file")
+    //     };
+    //     sys_mmap(
+    //         0x9000,
+    //         0x1000,
+    //         ProtFlags::PROT_EXEC.bits(),
+    //         MmapFlags::MAP_FILE.bits(),
+    //         fd as i64,
+    //         0,
+    //     );
 
-        let mut buffer = vec![0u8; 4096];
-        let bytes_read = {
-            fs.lock()
-                .read_file(fd, &mut buffer)
-                .await
-                .expect("Failed to read file")
-        };
-        debug!("bytes_read = {bytes_read}");
+    //     let mut buffer = vec![0u8; 4096];
+    //     let bytes_read = {
+    //         fs.lock()
+    //             .read_file(fd, &mut buffer)
+    //             .await
+    //             .expect("Failed to read file")
+    //     };
+    //     debug!("bytes_read = {bytes_read}");
 
-        let buf = &buffer[..bytes_read];
+    //     let buf = &buffer[..bytes_read];
 
-        let pid = create_process(buf);
-        schedule_process(pid);
-        let waiter = AwaitProcess::new(
-            pid,
-            get_runner_time(3_000_000_000),
-            current_running_event().unwrap(),
-        )
-        .await;
-        assert!(waiter.is_ok());
-    }
+    //     let pid = create_process(buf);
+    //     schedule_process(pid);
+    //     let waiter = AwaitProcess::new(
+    //         pid,
+    //         get_runner_time(3_000_000_000),
+    //         current_running_event().unwrap(),
+    //     )
+    //     .await;
+    //     assert!(waiter.is_ok());
+    // }
 }
